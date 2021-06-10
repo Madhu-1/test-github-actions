710e6f0d36944a5924be1a902bcb0ec43a8af50b
Madhu Rajanna <madhupr007@gmail.com>
2021-06-10 16:12:49 +0530

    do git config
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ docker login -u "${QUAY_IO_USERNAME}" -p "${GITHUB_TOKEN}" quay.io
 git clone https://github.com/ceph/csi-charts
 
 echo "done with cloning"
+git config user.name ceph-csi-bot
+git config user.email ceph-csi-bot@redhat.com
 git add --all :/ && git commit -m "Update for helm charts"
 echo "done with committing"
 git push https://"${GITHUB_TOKEN}"@github.com/ceph/csi-charts
