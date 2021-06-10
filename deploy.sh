dcea43a1411d84bd538202feb469aeff8ab8416c
Madhu Rajanna <madhupr007@gmail.com>
2021-06-10 16:01:41 +0530

    testing git push
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,11 @@ GITHUB_TOKEN=${GITHUB_TOKEN:-""}
 echo "Printing github token ${GITHUB_TOKEN}"
 
 docker login -u "${QUAY_IO_USERNAME}" -p "${GITHUB_TOKEN}" quay.io
+
+git clone https://github.com/ceph/csi-charts
+
+echo "done with cloning"
+git add --all :/ && git commit -m "Update for helm charts"
+echo "done with committing"
+git push https://"${GITHUB_TOKEN}"@github.com/ceph/csi-charts
+echo "done with pushing"
